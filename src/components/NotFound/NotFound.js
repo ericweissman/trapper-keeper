ec8401e9c1280d693ef4a02048853b00da5f93e0
Eric Weissman <eric.weissman21@gmail.com>
2019-02-11 16:49:01 -0700

    Adds NotFound component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const NotFound = () => {
+  return (
+    //add link back to home
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
