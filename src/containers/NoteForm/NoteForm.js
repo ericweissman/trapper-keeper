f6dafff4ad1bd8bcc309bdc1f5c7680f1abdbe29
Eric Weissman <eric.weissman21@gmail.com>
2019-02-11 16:48:28 -0700

    Adds NoteForm component

---
+++ src/containers/NoteForm/NoteForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class NoteForm extends Component {
+  constructor() {
+    super()
+    // this.state = {
+
+    // }
+  }
+
+  render() {
+    return(
+      <form>
+        
+      </form>
+    )
+  }
+}
+export default NoteForm
