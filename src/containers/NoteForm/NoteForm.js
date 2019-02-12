7010127c5f650a1582b5801811bebbef0e5bd42c
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-11 18:05:04 -0700

    Add local state for form

--- src/containers/NoteForm/NoteForm.js
+++ src/containers/NoteForm/NoteForm.js
@@ -3,15 +3,45 @@ import React, { Component } from 'react'
 export class NoteForm extends Component {
   constructor() {
     super()
-    // this.state = {
+    this.state = {
+      title: '',
+      items: [{ id: Date.now(), description: '' }]
+    }
+  }
 
-    // }
+  //  this.props.items || 
+
+  handleOnChange = (e) => {
+    const { name, value } = e.target
+    if (name === 'title') {
+      this.setState({ title: value })
+    } else {
+      const newItems = this.state.items.map(item => {
+        if (item.id === parseInt(name)) {
+          item.description = value
+        }
+        return item
+      })
+      this.setState({ items: newItems })
+    }
+  }
+
+  handleAddItem = (e) => {
+    e.preventDefault()
+    this.setState({ items: [...this.state.items, { id: Date.now(), description: '' }] })
   }
 
   render() {
-    return(
+    const { items, title } = this.state
+    return (
       <form>
-        
+        <input placeholder='Title' onChange={this.handleOnChange} name='title' value={title}></input>
+        {
+          items.map((item, i) => {
+            return <input placeholder='item' onChange={this.handleOnChange} name={item.id} value={items[i].description} key={item.id} ></input>
+          })
+        }
+        <button onClick={this.handleAddItem}>Add Item</button>
       </form>
     )
   }
