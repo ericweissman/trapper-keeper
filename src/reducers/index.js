c069430a56ef38dd85b54eba202d991b45a6f49b
Travis Gee <geet084@gmail.com>
2019-02-13 17:24:18 -0700

    Combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import { itemsReducer } from './itemsReducer'
+import { notesReducer } from './notesReducer'
 
 export const rootReducer = combineReducers({
-
+  items: itemsReducer,
+  notes: notesReducer,
 });
