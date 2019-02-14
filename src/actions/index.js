d86d68e3da4eb48d496cf938b8ab516deed0146f
Travis Gee <geet084@gmail.com>
2019-02-13 17:24:34 -0700

    Add actions for notes and items

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
 //add note
 //remove note
 //edit a note
+
+export const setNotes = (notes) => ({
+  type: 'SET_NOTES',
+  notes,
+})
+
+export const setItems = (items) => ({
+  type: 'SET_ITEMS',
+  items,
+})
