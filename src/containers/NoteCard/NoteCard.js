dd8f965551709a851eb84d89deb2970c5576df7e
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-14 14:28:02 -0700

    Add NoteCard Container

---
+++ src/containers/NoteCard/NoteCard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import NoteItem from '../NoteItem/NoteItem'
+import { Link } from 'react-router-dom'
+
+
+export class NoteCard extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    const cardItems = this.props.noteItems.map(item => {
+      return <NoteItem item={item} key={item.id} />
+    })
+
+    return (
+      <Link to={`/notes/${this.props.id}`}>
+        <div>
+          <h2>{this.props.title}</h2>
+          {cardItems}
+        </div>
+      </Link>
+    )
+  }
+}
+
+// export const mapStateToProps = (state) => ({
+
+// })
+
+
+export default NoteCard
