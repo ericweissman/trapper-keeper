c4d1a34d662c31d89dc2a6c1b62220c0405196bf
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-14 14:27:45 -0700

    Add noteArea container

---
+++ src/containers/NoteArea/NoteArea.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import NoteCard from '../NoteCard/NoteCard'
+import {connect} from 'react-redux'
+
+export class NoteArea extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    const { items, notes } = this.props
+
+    const noteCards = notes.map(note => {
+      const noteItems = items.filter(item => item.noteID === note.id)
+
+      return <NoteCard {...note} noteItems={noteItems} key={note.id}/>
+    })
+
+    return (
+      <main>
+        {noteCards}
+      </main>
+    )
+  }
+}
+
+export const mapStateToProps = (state) => ({
+  notes: state.notes,
+  items: state.items
+})
+
+export default connect(mapStateToProps)(NoteArea)
