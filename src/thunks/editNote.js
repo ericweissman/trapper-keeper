ef20ba655e4cedcfbedf4300b40d58d0d2291544
Eric Weissman <eric.weissman21@gmail.com>
2019-02-14 18:35:37 -0700

    Adds thunk for editing notes

---
+++ src/thunks/editNote.js
@@ -0,0 +1,23 @@
+import { isLoading, hasErrored, editNoteSuccess } from '../actions'
+
+export const editNote = (url, {id, title, items}) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, items })
+      })
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      dispatch(isLoading(false))
+      dispatch(editNoteSuccess({id, title, items}))
+    } catch (error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
+}
