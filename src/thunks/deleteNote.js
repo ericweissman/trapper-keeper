fe8f67c527e57aa0900f0439b2cafb3bf3d43096
Eric Weissman <eric.weissman21@gmail.com>
2019-02-14 18:36:00 -0700

    Adds thunk for deleting notes

---
+++ src/thunks/deleteNote.js
@@ -0,0 +1,24 @@
+import { isLoading, hasErrored, deleteNoteSuccess }  from '../actions'
+
+export const deleteNote = (url, id) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({id})
+      })
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      dispatch(isLoading(false))
+      // await response.json()
+      dispatch(deleteNoteSuccess(id))
+    } catch (error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
+}
