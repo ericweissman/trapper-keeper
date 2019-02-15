2a0357a91ab07dc7f682a7bb26baa94ab1ee9707
Eric Weissman <eric.weissman21@gmail.com>
2019-02-14 18:36:14 -0700

    Updates reducers for thunks

--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -1,11 +1,14 @@
 export const itemsReducer = (state = [], action) => {
   switch (action.type) {
-    case 'SET_ITEMS':
+    case 'GET_NOTES_SUCCESS':
       return action.items;
-    case 'ADD_NOTE_ITEMS':
+    case 'POST_NOTE_SUCCESS':
       return [...state, ...action.items]
-    case 'DELETE_NOTE_ITEMS':
+    case 'DELETE_NOTE_SUCCESS':
       return state.filter(item => item.noteID !== action.noteID)
+    case 'EDIT_NOTE_SUCCESS':
+      const cleaned = state.filter(item => item.noteID !== action.noteID)
+      return [...cleaned, ...action.items]
     default:
       return state;
   }
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,11 +1,14 @@
 export const notesReducer = (state = [], action) => {
   switch (action.type) {
-    case 'SET_NOTES':
+    case 'GET_NOTES_SUCCESS':
       return action.notes;  
-    case 'ADD_NOTE':
+    case 'POST_NOTE_SUCCESS':
       return [...state, action.note]
-      case 'DELETE_NOTE':
-        return state.filter(note => note.id !== action.id)
+    case 'DELETE_NOTE_SUCCESS':
+      return state.filter(note => note.id !== action.id)
+    case 'EDIT_NOTE_SUCCESS':
+      const cleaned = state.filter(note => note.id !== action.id)
+      return [...cleaned, ...action.notes]
     default:
       return state;
   }
