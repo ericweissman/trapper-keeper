3ba35b4bdda2db7426c0caa2b33375553e6c0af9
Eric Weissman <eric.weissman21@gmail.com>
2019-02-14 18:36:50 -0700

    Updates editForm for notes

--- src/containers/EditForm/EditForm.js
+++ src/containers/EditForm/EditForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { deleteNote, deleteNoteItems } from '../../actions'
 import { Link, Redirect } from 'react-router-dom'
+import { deleteNote } from '../../thunks/deleteNote'
+import { editNote } from '../../thunks/editNote'
 
 export class EditForm extends Component {
   constructor(props) {
@@ -12,47 +13,66 @@ export class EditForm extends Component {
         id: this.props.note.id
       },
       items: this.props.items,
-      isDeleted: false
+      isDeleted: false,
+      redirect: false,
     }
   }
 
   handleDelete = async () => {
-    this.props.deleteNote(this.state.note.id)
-    this.props.deleteNoteItems(this.state.note.id)
-    await fetch(`http://localhost:3001/api/v1/notes/${this.state.note.id}`, {
-      method: 'DELETE',
-      headers: { "Content-Type": "application/json" }
+    const { id } = this.state.note
+    const url = `http://localhost:3001/api/v1/notes/${id}`
+    this.props.deleteNote(url, id)
+    this.setState({
+      redirect: true,
     })
-
-    // await this.setState({isDeleted: true})
- 
   }
 
+  handleChange = (e) => {
+    const { id, title } = this.state.note
+    const { items } = this.state
+    const { name, value } = e.target
+    this.setState({
+      note: {title: value, id},
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { id, title } = this.state.note
+    const { items } = this.state
+    const url = `http://localhost:3001/api/v1/notes/${id}`
+    this.props.editNote(url, { id, title, items })
+    this.setState({
+      redirect: true,
+    })
+    this.props.edits()
+  }
 
   render() {
-    // if (this.state.isDeleted) {
-    //   return <Redirect to='/' />
-    // }
     const { title } = this.state.note
-    // const {items} = this.state
-
     const items = this.state.items.map(item => {
-      return <div>{item.description}</div>
+      return <input value={item.description} />
     })
 
     return (
       <div>
-        <Link to='/'><button onClick={this.handleDelete}>delete</button></Link>
-        {title}
+        <button onClick={this.handleDelete}>delete</button>
+        <input onChange={this.handleChange} name='title' value={title}/>
         {items}
+        <button onClick={this.handleSubmit}>Update Note</button>
+        {
+          this.state.redirect && <Redirect to='/'/>
+        }
       </div>
     )
   }
 }
 
 export const mapDispatchToProps = (dispatch) => ({
-  deleteNote: (id) => dispatch(deleteNote(id)),
-  deleteNoteItems: (noteID) => dispatch(deleteNoteItems(noteID))
+  // deleteNote: (id) => dispatch(deleteNote(id)),
+  // deleteNoteItems: (noteID) => dispatch(deleteNoteItems(noteID)),
+  deleteNote: (url, id) => dispatch(deleteNote(url, id)),
+  editNote: (url, note) => dispatch(editNote(url, note))
 })
 
 export default connect(null, mapDispatchToProps)(EditForm)
