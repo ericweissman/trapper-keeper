532328d79e928d9c725ba2310da6d2c37b87749a
Eric Weissman <eric.weissman21@gmail.com>
2019-02-14 18:37:11 -0700

    Adds correct imports and redux middleware

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { rootReducer } from '../src/reducers';
+import thunk from 'redux-thunk';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, devTools);
+// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
