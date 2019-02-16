dff3baa9224606e70d67bd515082e74f559bab1d
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-16 11:59:03 -0700

    Add isLoadingReducer, resolves #54

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { itemsReducer } from './itemsReducer'
 import { notesReducer } from './notesReducer'
+import { isLoadingReducer } from './isLoadingReducer'
+import { hasErroredReducer } from './hasErroredReducer'
 
 export const rootReducer = combineReducers({
   items: itemsReducer,
   notes: notesReducer,
+  isLoading: isLoadingReducer,
+  hasErrored: hasErroredReducer,
 });
--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -1,7 +1,6 @@
 export const itemsReducer = (state = [], action) => {
   switch (action.type) {
     case 'GET_NOTES_SUCCESS':
-    console.log(action)
       return action.items;
     case 'POST_NOTE_SUCCESS':
       return [...state, ...action.items]
