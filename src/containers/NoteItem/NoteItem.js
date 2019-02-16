320e570d2c848d8e30c45a241046b61226e346e3
Travis Gee <geet084@gmail.com>
2019-02-16 15:19:24 -0700

    Move individual items to their own component

--- src/containers/NoteItem/NoteItem.js
+++ src/containers/NoteItem/NoteItem.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 export class NoteItem extends Component {
+
+  deleteItem = () => {
+    this.props.handleItemDelete(this.props.item.id)
+  } 
+
   render() {
+    const { handleItemChange } = this.props;  
     return (
       <div>
-        {this.props.item.description}
+        <input type="checkbox"/>
+        <input
+          type="text"
+          name={this.props.item.id}
+          value={this.props.item.description}
+          onChange={handleItemChange}
+        />
+        <button onClick={this.deleteItem}>x</button>
       </div>
     )
   }
