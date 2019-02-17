82377cff9d47391657cf2e2540cd5918d20d17b0
Eric Weissman <eric.weissman21@gmail.com>
2019-02-17 12:15:40 -0700

    Adjusts items reducer based on noteID

--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -3,9 +3,9 @@ export const itemsReducer = (state = [], action) => {
     case 'GET_NOTES_SUCCESS':
       return action.items;
     case 'POST_NOTE_SUCCESS':
-      return [...state, ...action.items]
+    return [...state, ...action.items]
     case 'DELETE_NOTE_SUCCESS':
-      return state.filter(item => item.id !== action.id)
+      return state.filter(item => item.noteID !== action.id)
     case 'EDIT_NOTE_SUCCESS':
       const cleaned = state.filter(item => item.noteID !== action.note.id)
       return [...cleaned, ...action.items]
