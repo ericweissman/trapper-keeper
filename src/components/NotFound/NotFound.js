a0b559aea4586a314811b3dc21a608efa6370d2c
Eric Weissman <eric.weissman21@gmail.com>
2019-02-19 11:33:16 -0700

    Styles NotFound and cleans up other erroneous console.logs

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className='not-found'>
+      <h3>404 Not Found</h3>
       <Link to='/'>Return to All Notes</Link>
     </div>
   )
