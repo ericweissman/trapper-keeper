ca288b731ad026a8df14757d86c7ad459c725816
Travis Gee <geet084@gmail.com>
2019-02-19 14:20:12 -0700

    Remove checkboxes from main page

--- src/components/CompleteItem/CompleteItem.js
+++ src/components/CompleteItem/CompleteItem.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import checked from '../../images/checked.svg'
 import PropTypes from 'prop-types';
-
 
 export const CompleteItem = (props) => {
   return (
     <div className='completed'>
-      <img src={checked}/>
       <p>{props.description}</p>
     </div>
   )
--- src/components/IncompleteItem/IncompleteItem.js
+++ src/components/IncompleteItem/IncompleteItem.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import unchecked from '../../images/unchecked.svg'
 import PropTypes from 'prop-types';
 
 export const IncompleteItem = (props) => {
   return (
     <div className='incomplete'>
-      <img src={unchecked}/>
       <p>{props.description}</p>
     </div>
   )
